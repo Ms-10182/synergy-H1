ec4f4f392bc35274bcd6b4265d5cc50e3bae5970
Mayank Sharma <22bcs10182@cuchd.in>
2025-01-17 12:09:23 +0530

    added and updated imports and functions
    1) added roles - owners, minters
    2)added events for minter, owner, balance updated, token expired
    3) modified constructor
    4) setter for minter and owner
    5) updated updateExpired tokens

--- contract.sol
+++ contract.sol
@@ -1,24 +1,65 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Roles.sol";
 
 contract ExpirableToken is ERC20,ReentrancyGuard {
+
     struct TokenBatch {
         uint256 amount;
         uint256 expiration;
     }
 
     mapping(address => TokenBatch[]) private _tokenBatches;
+    using Roles for Roles.Role;
+    Roles.Role private _owners;
+    Roles.Role private _minters;
 
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
-
+    
     event TokensMinted(address indexed to, uint256 amount, uint256 expiration);
     event TokensBurned(address indexed account, uint256 amount);
     event TokensTransferred(address indexed from, address indexed to, uint256 amount, uint256 expiration);
+    event MinterAdded(address indexed by, address[] indexed minterAddress);
+    event OwnerAdded(address indexed by, address[] indexed OwnerAddress);
+    event BalanceUpdated(address indexed user,uint newAmount);
+    event TokensExpired(address indexed user,uint amount);
+
+
+    constructor(string memory name, string memory symbol,address[] memory minters,address [] memory owners) ERC20(name, symbol) {
+        _owners.add(msg.sender);
+        uint max = minters.length>owners.length?minters.length:owners.length;
+        for(uint i=0;i<max;i++){
+            if(i<minters.length){
+                _minters.add(minters[i]);
+            }
+            if(i<owners.length){
+                _owners.add(owners[i]);
+            }
+        }
+        emit OwnerAdded(msg.sender, owners);
+        emit MinterAdded(msg.sender, minters);
+    }
+
+    function addOwners(address [] memory newOwners) external {
+        require( _owners.has(msg.sender), "DOES_NOT_HAVE_OWNER_ROLE");
+        for (uint i=0;i<newOwners.length;i++){
+            _owners.add(newOwners[i]);
+        }
+            emit OwnerAdded(msg.sender, newOwners);
+    }
+    function addMinters(address [] memory newMinters) external {
+        require( _owners.has(msg.sender), "DOES_NOT_HAVE_OWNER_ROLE");
+        for (uint i=0;i<newMinters.length;i++){
+            _minters.add(newMinters[i]);
+        }
+        emit MinterAdded(msg.sender, newMinters);
+    }
+
 
     function mint(address to,uint256 amount,uint256 expiration) public {
+        require(_minters.has(msg.sender) || _owners.has(msg.sender), "DOES_NOT_HAVE_MINTER_ROLE");
         require(amount>0,"amount 0 or low");
         require(to!=address(0),"address is 0 can't mint");
         _mint(to, amount);
@@ -71,20 +112,25 @@ contract ExpirableToken is ERC20,ReentrancyGuard {
         return true;
     }
 
-    function _updateExpiredTokens(address account) internal   {
-        for (uint256 i = 0; i < _tokenBatches[account].length; ) {
-            if (block.timestamp >= _tokenBatches[account][i].expiration) {
-                if (_tokenBatches[account][i].amount > 0) {
-                    _burn(account, _tokenBatches[account][i].amount);
-                }
-                // Remove expired batch
-                _tokenBatches[account][i] = _tokenBatches[account][_tokenBatches[account].length - 1];
-                _tokenBatches[account].pop();
-            } else {
-                i++;
-            }
+    function _updateExpiredTokens(address account) internal {
+    uint256 expiredAmount = 0;
+    for (uint256 i = 0; i < _tokenBatches[account].length; ) {
+        if (block.timestamp >= _tokenBatches[account][i].expiration) {
+            expiredAmount += _tokenBatches[account][i].amount;
+            // Remove expired batch
+            _tokenBatches[account][i] = _tokenBatches[account][_tokenBatches[account].length - 1];
+            _tokenBatches[account].pop();
+        } else {
+            i++;
         }
     }
+    if (expiredAmount > 0) {
+        _burn(account, expiredAmount);
+        emit TokensExpired(account, expiredAmount);
+    }
+    emit BalanceUpdated(account, balanceOf(account));
+}
+
 
 
     function _availableBalance(address account)internal view returns (uint256){
